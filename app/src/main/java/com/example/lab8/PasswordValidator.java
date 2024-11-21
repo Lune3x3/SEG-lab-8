ffd21307d2b9f4045503a294d6dff72a5e8c7bf0
Lune3x3 <robsmith.exe@gmail.com>
2024-11-21 14:44:29 -0500

    fixed password validation

--- app/src/main/java/com/example/lab8/PasswordValidator.java
+++ app/src/main/java/com/example/lab8/PasswordValidator.java
@@ -5,15 +5,20 @@ public class PasswordValidator {
     public static boolean isValidPassword(String password) {
         if (password == null) return false;
 
+        char[] pswrd = password.toCharArray();
+
         boolean hasUpperCase = false;
         boolean hasLowerCase = false;
+        boolean hasNumber = false;
+        boolean longEnough = pswrd.length > 9;
 
-        for (char c : password.toCharArray()) {
+        for (char c : pswrd) {
             if (Character.isUpperCase(c)) hasUpperCase = true;
             if (Character.isLowerCase(c)) hasLowerCase = true;
+            if (Character.isDigit(c)) hasNumber = true;
 
             // If both conditions are met, the password is valid
-            if (hasUpperCase && hasLowerCase) return true;
+            if (hasUpperCase && hasLowerCase && hasNumber && longEnough) return true;
         }
         return false;
     }
